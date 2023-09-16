01cfc48696e371f4fcea656dedd62090e707b670
Qin Guan <qinguan20040914@gmail.com>
2023-09-17 07:31:01 +0800

    feat: health check

--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,3 +1,14 @@
-export default defineEventHandler((event) => {
-  return 1
-})
+import { sql } from 'drizzle-orm'
+
+export default defineLazyEventHandler(() => eventHandler(async (event) => {
+  try {
+    await event.context.database.run(sql`SELECT 1;`)
+    return { ok: true, now: new Date() }
+  }
+  catch (err) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error',
+    })
+  }
+}))
