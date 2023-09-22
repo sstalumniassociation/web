e68e2186dfbbb79d641c53b49623398258259423
Qin Guan <qinguan20040914@gmail.com>
2023-09-23 00:33:47 +0800

    chore: add issue

--- server/api/user/[id].get.ts
+++ server/api/user/[id].get.ts
@@ -1,3 +1,6 @@
+// In this case, the id passed in path params is the firebaseId
+// https://github.com/unjs/h3/issues/543
+
 export default defineProtectedEventHandler(async (event) => {
   if (event.context.firebaseId !== event.context.params!.id) {
     throw createError({
