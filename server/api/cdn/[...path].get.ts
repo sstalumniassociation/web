cfe0366b8ea91d5afc98c583fb6651447b55efcd
Qin Guan <qinguan20040914@gmail.com>
2023-09-26 00:19:44 +0800

    fix(server): remove auth on cdn

--- server/api/cdn/[...path].get.ts
+++ server/api/cdn/[...path].get.ts
@@ -1,6 +1,6 @@
 import type { R2Bucket } from '@cloudflare/workers-types'
 
-export default defineProtectedEventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   const bucket: R2Bucket = event.context.cloudflare.env.R2_SSTAA
   const file = await bucket.get(event.context.params!.path)
 
@@ -14,11 +14,8 @@ export default defineProtectedEventHandler(async (event) => {
   const headers = new Headers()
   file.writeHttpMetadata(headers as any)
   headers.set('etag', file.httpEtag)
-  setResponseHeaders(event, Object.fromEntries(headers.entries()))
+  setHeaders(event, Object.fromEntries(headers.entries()))
+  setHeader(event, 'Cache-Control', `max-age=${60 * 60}`) // 1 hour cache
 
   return file.body
-}, {
-  cache: {
-    maxAge: 60 * 60,
-  },
 })
