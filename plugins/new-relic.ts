6d1464434aabd21468129c516b8af2877d89fc6e
Qin Guan <qinguan20040914@gmail.com>
2024-01-30 17:09:50 +0800

    chore: update new-relic agent id

--- plugins/new-relic.ts
+++ plugins/new-relic.ts
@@ -6,7 +6,7 @@ export default defineNuxtPlugin(() => {
   const options = {
     init: { distributed_tracing: { enabled: true }, privacy: { cookies_enabled: true }, ajax: { deny_list: ['bam.eu01.nr-data.net'] } },
     info: { beacon: 'bam.eu01.nr-data.net', errorBeacon: 'bam.eu01.nr-data.net', licenseKey: 'NRJS-1b1f41c1d6baafe183c', applicationID: config.newRelic.applicationId, sa: 1 },
-    loader_config: { accountID: '4337277', trustKey: '4337277', agentID: '538559115', licenseKey: 'NRJS-1b1f41c1d6baafe183c', applicationID: config.newRelic.applicationId },
+    loader_config: { accountID: '4337277', trustKey: '4337277', agentID: '538559409', licenseKey: 'NRJS-1b1f41c1d6baafe183c', applicationID: config.newRelic.applicationId },
   }
 
   const _ = new BrowserAgent(options)
